407784617b48f869f60fc8406451f0269992b197
Flavio Ortellao <flvio.ortellao@valtech.com>
2025-10-22 21:40:23 -0300

    properties parametrization

--- src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
+++ src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
@@ -3,17 +3,26 @@ package com.integrationninjas.springbootexample.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    private final Environment environment;
+
+    @Autowired
+    public TestController(Environment environment) {
+        this.environment = environment;
+    }
+
 	@GetMapping
 	public Object hello() {
 		Map<String, String> object = new HashMap<>();
-		object.put("name", "Integration Ninjas");
-		object.put("email", "integrationninjas@gmail.com");
+		object.put("name", environment.getProperty("example.api.name", "Integration Ninjas"));
+		object.put("email", environment.getProperty("example.api.email", "integrationninjas@gmail.com"));
 		return object;
 	}
 
