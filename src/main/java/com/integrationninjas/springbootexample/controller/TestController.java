dc0ed40ebd666300331dc73c4712a3d8fa20dc00
Flavio Ortellao <flvio.ortellao@valtech.com>
2025-10-22 22:09:37 -0300

    logging

--- src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
+++ src/main/java/com/integrationninjas/springbootexample/controller/TestController.java
@@ -3,6 +3,8 @@ package com.integrationninjas.springbootexample.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,8 @@ public class TestController {
 
     private final Environment environment;
 
+    private static final Logger logger = LoggerFactory.getLogger(TestController.class);
+
     @Autowired
     public TestController(Environment environment) {
         this.environment = environment;
@@ -20,6 +24,7 @@ public class TestController {
 
 	@GetMapping
 	public Object hello() {
+        logger.atInfo().log("Saying Hello");
 		Map<String, String> object = new HashMap<>();
 		object.put("name", environment.getProperty("example.api.name", "Integration Ninjas"));
 		object.put("email", environment.getProperty("example.api.email", "integrationninjas@gmail.com"));
